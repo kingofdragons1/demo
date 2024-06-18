1c4883c1162bdce270bbbff4be979127688dd214
Alex Panin <bybybymaiyami@yandex.ru>
2024-06-18 12:14:19 +0300

    Задание 14

---
+++ src/main/java/com/example/restdemo/PersonController.java
@@ -0,0 +1,61 @@
+package com.example.restdemo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+public class PersonController {
+
+	private List<Person> persons = new ArrayList<>(Arrays.asList(
+			new Person(1, "Ivan", "Ivanovich", "Ivanov", LocalDate.of(1999, 2,3)),
+			new Person(2, "Петр", "Петрович", "Петров", LocalDate.of(2002, 2,2)),
+			new Person(3, "Евгений", "Васильевич", "Васин", LocalDate.of(2005, 4,8)),
+			new Person(4, "Максим", "Яковлевич", "Окопский", LocalDate.of(1978, 6,5))
+	));
+
+	@PostMapping("/person")
+	public Person addPerson(@RequestBody Person person) {
+		persons.add(person);
+		return person;
+	}
+
+	@PutMapping("/person/{id}")
+	public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
+		int index = - 1;
+		for (Person p : persons) {
+			if (p.getId() == id) {
+				index = persons.indexOf(p);
+				persons.set(index, person);
+			}
+		}
+
+		return index == -1
+				? new ResponseEntity<>(addPerson(person), HttpStatus.CREATED)
+				: new ResponseEntity<>(person, HttpStatus.OK);
+	}
+
+	@GetMapping("/person/{id}")
+	public Optional<Person> findPersonById(@PathVariable int id) {
+		return persons.stream().filter(p -> p.getId() == id).findFirst();
+	}
+
+	@DeleteMapping("/person/{id}")
+	public void deletePerson(@PathVariable int id) {
+		persons.removeIf(p -> p.getId() == id);
+	}
+}
